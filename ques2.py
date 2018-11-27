074fc32fd31978ed3fd1e9d3234fdc58e5ee33d2
Hariharan Vijayachandran <hariv@Hariharans-MacBook-Pro.local>
2018-11-27 15:46:42 -0500

    ques2 done

--- ques2.py
+++ ques2.py
@@ -1,7 +1,9 @@
 import numpy as np
 import math
+def proj(a,b):
+	return (np.dot(np.array(a),b)/np.dot(b,b))*b
 
-class1 = np.array([[-2,1],[-5,4],[-3,1],[0,-3],[-8,-1]])
+class1 = np.array([[-2,1],[-5,-4],[-3,1],[0,-3],[-8,-1]])
 class2 = np.array([[2,5],[1,0],[5,-1],[-1,-3],[6,1]])
 
 
@@ -29,6 +31,22 @@ print(scatter_within)
 V_optimal_line = np.dot(np.linalg.inv(scatter_within),(class1_mean-class2_mean))
 print("optimal line vector: ")
 print(V_optimal_line)
+print("---------------")
+
+
+class1_projection = np.array(np.dot(V_optimal_line,class1.T))
+
+class2_projection = np.array(np.dot(V_optimal_line,class2.T))
+
+for i in range(len(class1_projection)):
+	if(class1_projection[i]<0):
+		
+		print("point "+ str(class1[i])+" was incorrectly projected")
+for i in range(len(class2_projection)):
+	if(class2_projection[i]>0):
+		
+		print("point "+ str(class2[i])+" was incorrectly projected")
+
 
 
 
